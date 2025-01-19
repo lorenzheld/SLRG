073758691f0877bf5a2ffa5b783aa2df186d5234
Lorenz Held <held.lorenz.08@gmail.com>
2025-01-19 14:31:25 +0100

    inserted baderegel video

--- src/Baderegeln.jsx
+++ src/Baderegeln.jsx
@@ -1,11 +1,48 @@
-import React from "react";
-import BildBaderegeln from "./images/baderegeln-dfi.jpg"
+import React, { useState } from "react";
+import BildBaderegeln from "./images/baderegeln-dfi.jpg";
+import Video from "./video/Baderegeln_Alle.mp4";
 
 export default function Baderegeln() {
-    return(
+    const [activeTab, setActiveTab] = useState('Text');
+
+    const handleTabClick = (tab) => setActiveTab(tab);
+
+    return (
         <>
             <h1 className="title">Baderegeln</h1>
-            <img src={BildBaderegeln} height="100%" width="100%" />
+            <div className="navbar">
+                <div
+                    className={activeTab === 'Text' ? 'active-tab' : 'tab'}
+                    onClick={() => handleTabClick('Text')}
+                >
+                    Text
+                </div>
+                <div
+                    className={activeTab === 'Video' ? 'active-tab' : 'tab'}
+                    onClick={() => handleTabClick('Video')}
+                >
+                    Video
+                </div>
+            </div>
+            <div className="tab-content">
+                {activeTab === 'Text' && (
+                    <>
+                        <h2>Baderegeln</h2>
+                        <img src={BildBaderegeln} alt="Baderegeln" height="100%" width="100%" />
+                    </>
+                )}
+                {activeTab === 'Video' && (
+                    <div className="video-container">
+                        <iframe
+                            src={Video}
+                            title="Baderegeln Video"
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        ></iframe>
+                    </div>
+                )}
+            </div>
         </>
-        );
+    );
 }
